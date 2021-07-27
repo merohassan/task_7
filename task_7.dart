924f7656bd77127b78e5beab2c7e5bf09136da2f
merohassan <87802413+merohassan@users.noreply.github.com>
2021-07-27 17:16:46 +0200

    Add files via upload

---
+++ task_7.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  // for loop to print all multiplication table
+  for(int i=1;i<=10;i++){
+    for(int x=1;x<=10;x++){
+      print ("$i * $x = ${i*x}");
+    }
+  }
+}
